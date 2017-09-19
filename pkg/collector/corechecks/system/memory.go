496ff17bd589919b09f80381a4437a076a0a1a48
Derek Brown <derek.brown@datadoghq.com>
2017-09-18 20:26:12 -0700

    [windows] fix paging stats on windows. Requires PR (submitted but not approved) to psutil

--- pkg/collector/corechecks/system/memory.go
+++ pkg/collector/corechecks/system/memory.go
@@ -68,8 +68,8 @@ func (c *MemoryCheck) Run() error {
 	v, errVirt := virtualMemory()
 	if errVirt == nil {
 		sender.Gauge("system.mem.total", float64(v.Total)/mbSize, "", nil)
-		sender.Gauge("system.mem.free", float64(v.Free)/mbSize, "", nil)
-		sender.Gauge("system.mem.used", float64(v.Total-v.Free)/mbSize, "", nil)
+		sender.Gauge("system.mem.free", float64(v.Available)/mbSize, "", nil)
+		sender.Gauge("system.mem.used", float64(v.Total-v.Available)/mbSize, "", nil)
 		sender.Gauge("system.mem.usable", float64(v.Available)/mbSize, "", nil)
 		sender.Gauge("system.mem.pct_usable", float64(100-v.UsedPercent)/100, "", nil)
 
