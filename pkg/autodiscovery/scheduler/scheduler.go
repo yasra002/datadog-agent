84074cf5296b1a35261703f05ab5af511b1e29dd
Pierre <mfpierre@gmail.com>
2018-05-29 17:35:38 +0200

    Add scheduler catalog

---
+++ pkg/autodiscovery/scheduler/scheduler.go
@@ -0,0 +1,34 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package scheduler
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
+)
+
+// MetaScheduler is a scheduler dispatching to all registered schedulers
+type MetaScheduler struct{}
+
+// ScheduleConfigs schedules configs to all registered schedulers
+func (s *MetaScheduler) ScheduleConfigs(configs []integration.Config) {
+	for _, scheduler := range DefaultCatalog {
+		scheduler.ScheduleConfigs(configs)
+	}
+}
+
+// UnscheduleConfigs unschedules configs to all registered schedulers
+func (s *MetaScheduler) UnscheduleConfigs(configs []integration.Config) {
+	for _, scheduler := range DefaultCatalog {
+		scheduler.UnscheduleConfigs(configs)
+	}
+}
+
+// Stop handles clean stop of registered schedulers
+func (s *MetaScheduler) Stop() {
+	for _, scheduler := range DefaultCatalog {
+		scheduler.Stop()
+	}
+}
