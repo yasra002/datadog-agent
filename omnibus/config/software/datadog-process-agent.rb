93d098ff8a930415664b477d2d9672164ee53160
Sunny Klair <sunny.klair@datadoghq.com>
2018-09-24 22:52:46 -0400

    Add licenses for network-tracer and process-agent

--- omnibus/config/software/datadog-process-agent.rb
+++ omnibus/config/software/datadog-process-agent.rb
@@ -16,6 +16,7 @@ default_version process_agent_version
 url = "https://s3.amazonaws.com/datad0g-process-agent/"
 
 build do
+  ship_license "https://raw.githubusercontent.com/DataDog/datadog-process-agent/#{version}/LICENSE"
   if windows?
     binary = "process-agent-windows-#{version}.exe"
     target_binary = "process-agent.exe"
@@ -31,6 +32,7 @@ build do
     command "mv #{binary} #{install_dir}/embedded/bin/#{target_binary}"
 
     # network-tracer versions will always be the same on both process-agent and network-tracer
+    ship_license "https://raw.githubusercontent.com/DataDog/tcptracer-bpf/#{version}/LICENSE.network-tracer"
     binary = "network-tracer-amd64-#{version}"
     target_binary = "network-tracer"
     curl_cmd = "curl -f #{url}#{binary} -o #{binary}"
