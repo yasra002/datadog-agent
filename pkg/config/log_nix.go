f5d05c53610b3aefe113c58836e75cd98509ac18
Rémi Calixte <remi.calixte@datadoghq.com>
2018-10-04 16:32:52 -0400

    Use $JOURNAL_STREAM to know if we are being ran by systemd

--- pkg/config/log_nix.go
+++ pkg/config/log_nix.go
@@ -8,21 +8,26 @@
 package config
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func init() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGPIPE)
-
-	go func() {
-		for s := range c {
-			fmt.Println("Got Signal:", s)
-		}
-	}()
+	// By default systemd redirect the stdout to journald. When journald is stopped or crashes we receive a SIGPIPE signal.
+	// Go ignore SIGPIPE signals unless it is when stdout or stdout is closed, in this case the agent is stopped. We don't want that
+	// so we intercept the SIGPIPE signals and just discard them.
+	// systemd 231+ set the $JOURNAL_STREAM env variable when it is redirecting stdout to journald.
+	// for older version of systemd, a workaround is to use the syslog integration instead of relying on stdout
+	if _, ok := os.LookupEnv("JOURNAL_STREAM"); ok {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGPIPE)
+		go func() {
+			for range c {
+				// do nothing
+			}
+		}()
+	}
 }
 
 // GetSyslogURI returns the configured/default syslog uri
